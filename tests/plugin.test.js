4824189ac0bc8eb2092fb2b2f5cea1952e3873fd
geyuanjun <geyuanjun@bilibili.com>
2022-04-18 23:29:48 +0800

    fix: update lighthouse to newest verison

--- tests/plugin.test.js
+++ tests/plugin.test.js
@@ -37,6 +37,7 @@ test('Collects network records and summarises them', async () => {
     'other',
     'total',
   ];
+
   const resourceTypes = jsonpath.query(
     lhrResult,
     "$['audits']['resource-granular-summary']['details']['items']..['resourceType']",
